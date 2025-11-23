24bee09180bc373cb7a55d1e94f9d0340730b4dc
Nanami131 <2920383858@qq.com>
2025-11-23 13:53:56 +0800

    用户播放事件消息发送

--- common/src/main/java/org/L2/common/constant/EventType.java
+++ common/src/main/java/org/L2/common/constant/EventType.java
@@ -1,4 +1,9 @@
 package org.L2.common.constant;
 
 public class EventType {
+
+    // 用户播放事件
+    public static final String EVENT_CATEGORY_PLAYBACK = "PLAYBACK";
+    public static final String EVENT_NAME_SONG_PLAY = "SONG_PLAY";
+
 }
--- common/src/main/java/org/L2/common/mq/PlayRecordProducer.java
+++ common/src/main/java/org/L2/common/mq/PlayRecordProducer.java
@@ -2,9 +2,18 @@ package org.L2.common.mq;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.L2.common.constant.EventType;
+import org.L2.common.event.EventInfo;
+import org.L2.common.event.PlaybackEventMessage;
+import org.L2.common.event.UserInfo;
 import org.L2.common.mq.config.RabbitMQConfig;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
 
 @Component
 @RequiredArgsConstructor
@@ -13,21 +22,40 @@ public class PlayRecordProducer {
 
     private final RabbitTemplate rabbitTemplate;
 
+    /**
+     * 发送播放事件到 MQ。
+     */
     public void sendPlayRecord(Long userId) {
-        PlayRecordMessage message = new PlayRecordMessage()
+        // 组装事件元信息
+        EventInfo eventInfo = new EventInfo()
+                .setEventId(UUID.randomUUID().toString()) // todo
+                .setEventCategory(EventType.EVENT_CATEGORY_PLAYBACK)
+                .setEventName(EventType.EVENT_NAME_SONG_PLAY)
+                .setOccurredAt(LocalDateTime.now())
+                .setTraceId(UUID.randomUUID().toString()); // todo
+
+        UserInfo userInfo = new UserInfo()
                 .setUserId(userId);
+
+        // 顶层消息
+        PlaybackEventMessage message = new PlaybackEventMessage()
+                .setEvent(eventInfo)
+                .setUser(userInfo);
+
         try {
-            log.info("Sending play record message to RabbitMQ, userId={}", userId);
+            log.info("Sending playback event to RabbitMQ, userId={}, eventId={}",
+                    userId, eventInfo.getEventId());
             rabbitTemplate.convertAndSend(
                     RabbitMQConfig.PLAY_RECORD_EXCHANGE,
                     RabbitMQConfig.PLAY_RECORD_ROUTING_KEY,
                     message
             );
-            log.info("Play record message sent successfully, userId={}", userId);
+            log.info("Playback event sent successfully, userId={}, eventId={}",
+                    userId, eventInfo.getEventId());
         } catch (Exception e) {
-            log.error("Failed to send play record message, userId={}", userId, e);
+            log.error("Failed to send playback event, userId={}, eventId={}",
+                    userId, eventInfo.getEventId(), e);
             throw e;
         }
     }
 }
-
