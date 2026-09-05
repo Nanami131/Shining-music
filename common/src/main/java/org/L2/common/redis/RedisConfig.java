63e9e561dcd659d4ecc1d4a7ea28754cf25143a6
hachimi <email@email>
2026-09-05 15:30:11 +0800

    fix:修复序列化bug

--- common/src/main/java/org/L2/common/redis/RedisConfig.java
+++ common/src/main/java/org/L2/common/redis/RedisConfig.java
@@ -3,6 +3,7 @@ package org.L2.common.redis;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
@@ -12,9 +13,22 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 @Configuration
 public class RedisConfig extends CachingConfigurerSupport {
+
+    private static final DateTimeFormatter LOCAL_DATE_TIME_OUTPUT_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private static final DateTimeFormatter LOCAL_DATE_TIME_INPUT_FORMATTER =
+            new DateTimeFormatterBuilder()
+                    .parseStrict()
+                    .append(DateTimeFormatter.ISO_LOCAL_DATE)
+                    .appendPattern("[ HH:mm:ss]['T'HH:mm:ss]")
+                    .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
+                    .toFormatter();
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
@@ -28,9 +42,10 @@ public class RedisConfig extends CachingConfigurerSupport {
         ObjectMapper objectMapper = new ObjectMapper();
 
         JavaTimeModule javaTimeModule = new JavaTimeModule();
-        // 指定 LocalDateTime 的输出格式为 "yyyy-MM-dd HH:mm:ss"
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(formatter));
+        javaTimeModule.addSerializer(LocalDateTime.class,
+                new LocalDateTimeSerializer(LOCAL_DATE_TIME_OUTPUT_FORMATTER));
+        javaTimeModule.addDeserializer(LocalDateTime.class,
+                new LocalDateTimeDeserializer(LOCAL_DATE_TIME_INPUT_FORMATTER));
 
         objectMapper.registerModule(javaTimeModule);
         // 关闭时间戳形式输出，使用上面的字符串格式
